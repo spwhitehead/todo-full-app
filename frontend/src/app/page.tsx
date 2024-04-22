57520805700f34a1a4e0f604a1c35985fd80bf9f
Spencer Whitehead <53270623+spwhitehead@users.noreply.github.com>
2024-04-22 09:07:57 -0600

    Restarts all frontend stuff

--- frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-fetch("http://localhost:8000/", {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    }).then(fetchTodos);
-
-export default function Home() {
-  return <p>Hello World!</p>
-}
